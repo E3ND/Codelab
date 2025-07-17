525ffd9daf6d22a7b646f767ba5b8d6196fe9c9b
E3ND <brunopimentelcityhotmail.com@gmail.com>
2025-07-17 12:55:04 -0300

    create nav menu

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
+//npm install -D @svgr/webpack para poder importar assets como se fosse componentes
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      }
+    }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/this.webpack",
+          options: {
+            icon: true
+          }
+        }
+      ]
+    })
+
+    return config;
+  }
 };
 
 export default nextConfig;
