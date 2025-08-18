6784fc58ee2d0f19903100fde63f0aec6116393c
E3ND <brunopimentelcityhotmail.com@gmail.com>
2025-08-18 15:46:23 -0300

    ✨ Update dependencies in package.json: - Update "@radix-ui/react-dialog" to version 1.1.15 - Add "@radix-ui/react-label" version 2.1.7
    ✨ Add new components and assets:
    - Add "pix.svg" to src/assets
    - Add "credit-card.tsx" to src/components/pages/courses/course-details/checkout-dialog
    - Add "index.tsx" to src/components/pages/courses/course-details/checkout-dialog
    - Add "course-progress.tsx" to src/components/pages/courses/course-details
    - Add "index.tsx" to src/components/ui/dialog
    
    🐛 Fix logic in "course-progress.tsx":
    - Change initial value of "hasCourse" to false
    - Add functionality to show checkout dialog when clicking on "Comprar" button
    
    🔧 Add new component "Dialog" to create customizable dialog boxes in the UI
    
    ✨ (primitives.tsx): Add new dialog primitives components for creating customizable dialogs in React applications
    ✨ (form.tsx): Add new form components for creating customizable forms in React applications
    
    ✨ (primitives.tsx): Add new form primitives components for building forms in React applications
    📝 (label.tsx): Add new Label component to be used in form primitives for labeling form fields
    📝 (payment.ts): Add creditCardCheckoutFormSchema to define schema for credit card checkout form
    🔧 (globals.css): Update plyr-border-radius to 0px for consistent styling across the application

---
+++ src/components/pages/courses/course-details/checkout-dialog/credit-card.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form/primitives"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+
+export const CreditCard = () => {
+    return (
+        <Form>
+            <form className="flex flex-col">
+                <div className="flex items-center gap-4 flex-col">
+                    <div>
+                        <p>Preview</p>
+                    </div>
+
+                    <div className="w-full grid sm:grid-cols-2 gap-2 flex-1">
+                        <p>INPUTS</p>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-between mt-6">
+                    <Button variant="outline">
+                        <ArrowLeft />
+                        Voltar
+                    </Button>
+
+                    <Button type="submit">
+                        Confirmar
+                        <ArrowRight />
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    )
+}
--- src/components/pages/courses/course-details/course-progress.tsx
+++ src/components/pages/courses/course-details/course-progress.tsx
@@ -8,13 +8,16 @@ import { formatPrice } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { Play, ShoppingCart } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+import { CheckoutDialog } from "./checkout-dialog";
 
 type CourseProgressProps = {
     course: Course;
 }
 
 export const CourseProgress = ({ course }: CourseProgressProps) => {
-    const hasCourse = true;
+    const [showCheckoutDialog, setShowCheckoutDialog] = useState(false);
+    const hasCourse = false;
 
     const { data: courseProgress } = useQuery({
         queryKey: queryKeys.CourseProgress(course.slug),
@@ -57,12 +60,14 @@ export const CourseProgress = ({ course }: CourseProgressProps) => {
                         {formatPrice(course.discountPrice ?? course.price)}
                     </p>
 
-                    <Button className="w-full mt-2 text-xl font-bold h-auto text-white py-3">
+                    <Button className="w-full mt-2 text-xl font-bold h-auto text-white py-3" onClick={() => setShowCheckoutDialog(true)}>
                         Comprar
                         <ShoppingCart />
                     </Button>
                 </div>
             )}
+
+            <CheckoutDialog open={showCheckoutDialog} setOpen={setShowCheckoutDialog} course={course} />
         </aside>
     )
 }
